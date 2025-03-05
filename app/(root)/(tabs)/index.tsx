86abfa02d0a8f581a6863418d06e007871e4891d
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-03-05 23:47:43 +0545

    tailwind config

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-
+      <Text className="font-bold text-lg my-10">Welcom to ReState</Text>
       <Link href="/sign-in">SignIn</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
