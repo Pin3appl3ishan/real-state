82ad6f7f5d53089942f42d49f11b2ac2905a8002
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-03-18 23:51:29 +0545

    feat: wrap RootLayout in GlobalProvider for improved context management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { SplashScreen, Stack } from "expo-router";
 import "./globals.css";
-import {useFonts} from "expo-font";
+import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -22,6 +23,10 @@ export default function RootLayout() {
   if (!fontsLoaded) {
     return null;
   }
-  
-  return <Stack screenOptions={{headerShown: false}}/>;
+
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GlobalProvider>
+  );
 }
