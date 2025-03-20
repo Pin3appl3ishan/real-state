f62c407fbb6ed783279fa140a79dafd7cf9b1a43
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-03-20 23:42:07 +0545

    feat: create SettingsItem component for reusability

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,43 +1,73 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  ImageSourcePropType,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 
-import icons from '@/constants/icons';
-import images from '@/constants/images';
+interface SettingsItemProps {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
+}
+
+const SettingsItem = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: SettingsItemProps) => (
+  <TouchableOpacity>
+    <View>
+      <Image source={icon} />
+      <Text>{title}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
 const Profile = () => {
-  const handleLogout = async ()  => {};
+  const handleLogout = async () => {};
 
   return (
     <SafeAreaView>
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerClassName='pb-32 px-7'
+        contentContainerClassName="pb-32 px-7"
       >
-        <View className='flex flex-row justify-between items-center mt-5'>
-          <Text className='text-xl font-rubik-bold'>
-            Profile
-          </Text>
-          <Image source={icons.bell} className='size-5'/>
+        <View className="flex flex-row justify-between items-center mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
         </View>
 
-        <View className='flex-row justify-center flex mt-5'>
-          <View className='felx flex-col items-center relative mt-5'>
-            <Image source={images.avatar} className='size-44 relative rounded-full'/>
-            <TouchableOpacity className='absolute bottom-11 right-2'>
-              <Image source={icons.edit} className='size-9'/>
+        <View className="flex-row justify-center flex mt-5">
+          <View className="felx flex-col items-center relative mt-5">
+            <Image
+              source={images.avatar}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
             </TouchableOpacity>
 
-            <Text className='text-2xl font-rubik-bold mt-2'>Ishan Shrestha</Text>
+            <Text className="text-2xl font-rubik-bold mt-2">
+              Ishan Shrestha
+            </Text>
           </View>
         </View>
 
-        <View className='flex flex-col mt-10'>
-
-        </View>
+        <View className="flex flex-col mt-10"></View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
