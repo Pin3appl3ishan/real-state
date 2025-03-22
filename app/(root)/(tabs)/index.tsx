baa42b7001d95da1a942dda284c0d8a4802de8b7
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-03-22 23:46:55 +0545

    feat: enhance Index component with user greeting and avatar display

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,28 @@
+import Search from "@/components/Search";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
-
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl font-rubik my-10">Welcome to RealState</Text>
-    </View>
+    <SafeAreaView>
+      <View className="bg-white h-full">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning!
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Ishan
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      
+    </SafeAreaView>
   );
 }
