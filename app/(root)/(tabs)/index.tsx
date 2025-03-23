f6abacbbb1c28e657b2d6d99ec50423dc572916d
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-03-23 23:31:03 +0545

    feat: enhance Index component with featured section and improved layout

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import Search from "@/components/Search";
 import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <View className="bg-white h-full">
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row items-center">
             <Image source={images.avatar} className="size-12 rounded-full" />
@@ -21,8 +21,20 @@ export default function Index() {
             </View>
           </View>
         </View>
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-      
     </SafeAreaView>
   );
 }
