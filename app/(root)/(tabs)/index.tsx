bbe06c3145945a5b6b4dc9fa2d2ecfcdfeb4d622
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-03-24 00:10:04 +0545

    feat: add FeaturedCard and Card components to index

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import images from "@/constants/images";
 import { Link } from "expo-router";
@@ -33,6 +34,28 @@ export default function Index() {
               </Text>
             </TouchableOpacity>
           </View>
+
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Our Recommendation
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex flow-row gap-5 mt-5">
+          <Card />
+          <Card />
         </View>
       </View>
     </SafeAreaView>
