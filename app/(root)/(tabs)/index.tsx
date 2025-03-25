1b02279c32f2d2ee412528f4443febf7314ad546
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-03-25 23:52:06 +0545

    feat: enhance Index component with user-specific avatar and name, and add horizontal FeaturedCard list

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,22 +1,30 @@
 import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import images from "@/constants/images";
+import { useGlobalContext } from "@/lib/global-provider";
 import { Link } from "expo-router";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const {user} = useGlobalContext();;
+  
   return (
     <SafeAreaView className="bg-white h-full">
       <FlatList
-        data={[1, 2, 3]}
+        data={[1, 2]}
         renderItem={({ item }) => <Card />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
         ListHeaderComponent={
           <View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
               <View className="flex flex-row items-center">
                 <Image
-                  source={images.avatar}
+                  source={{uri: user?.avatar}}
                   className="size-12 rounded-full"
                 />
                 <View className="flex flex-col items-start ml-2 justify-center">
@@ -24,7 +32,7 @@ export default function Index() {
                     Good Morning!
                   </Text>
                   <Text className="text-base font-rubik-medium text-black-300">
-                    Ishan
+                    {user?.name}
                   </Text>
                 </View>
               </View>
@@ -41,6 +49,15 @@ export default function Index() {
                   </Text>
                 </TouchableOpacity>
               </View>
+
+              <FlatList
+                data={[1, 2, 3]}
+                renderItem={({ item }) => <FeaturedCard />}
+                keyExtractor={(item) => item.toString()}
+                horizontal
+                bounces={false}
+                showsHorizontalScrollIndicator={false}
+                contentContainerClassName="flex gap-5 mt-5"/>
             </View>
 
             <View className="flex flex-row items-center justify-between">
